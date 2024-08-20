e98d8093c2580aa37a19fa6c150f1f680d07de9d
정호연 <jea7730@gmail.com>
2024-08-20 17:01:21 +0900

    :sparkles: 정호연 : item 상태 관리

--- front/src/components/GaugeBar.tsx
+++ front/src/components/GaugeBar.tsx
@@ -16,6 +16,13 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
   const [isInventoryOpen, setIsInventoryOpen] = useState(false);
   const [showItems, setShowItems] = useState(false);
   const [inventory, setInventory] = useState<string[]>([]); // 인벤토리 상태 관리
+  const [visibleItems, setVisibleItems] = useState<string[]>([
+    'chick',
+    'nest',
+    'ribbon',
+    'sunglasses-1',
+    'sunglasses-2',
+  ]); // 화면에 보이는 아이템들 관리
 
   useEffect(() => {
     if (gauge === 0) {
@@ -28,7 +35,15 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
   // 아이템을 인벤토리에 추가하는 함수
   const addItemToInventory = (itemName: string) => {
     setInventory((prevInventory) => [...prevInventory, itemName]);
-    setShowItems(false); // 아이템 클릭 후 화면에서 아이템 제거
+    setVisibleItems((prevVisibleItems) => prevVisibleItems.filter((item) => item !== itemName));
+
+    // 모든 아이템이 수집되면 추가적인 동작 수행 (예: 화면 리셋)
+    if (visibleItems.length === 1) {
+      // 모든 아이템이 수집된 후 동작
+      setTimeout(() => {
+        window.location.reload(); // 예: 페이지를 리로드
+      }, 1000);
+    }
   };
 
   return (
@@ -37,7 +52,8 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
         <div className="font-bold mb-4">Lv.99 욱재몬</div>
         <Gauge gauge={gauge} />
         {!showItems && <Kong onClick={handleClick} isShaking={isShaking} />}
-        {showItems && <ItemsList onItemClicked={addItemToInventory} />} {/* 아이템 클릭 핸들러 전달 */}
+        {showItems && <ItemsList visibleItems={visibleItems} onItemClicked={addItemToInventory} />}{' '}
+        {/* 수정된 아이템 리스트 */}
       </div>
       <Image
         src={bag}
