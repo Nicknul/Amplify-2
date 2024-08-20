2f2e31c04b64bf87fa1090289ffe43766eb64b77
정호연 <jea7730@gmail.com>
2024-08-20 11:29:48 +0900

    :hammer: 정호연 : Modal Module 조립

--- front/src/components/GaugeBar.tsx
+++ front/src/components/GaugeBar.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useGauge } from '../hooks/useGauge';
+import Modal from './Modal';
 
 type GaugeBarProps = {
   initialValue: number;
 };
 
 const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
-  const { gauge, decreaseGauge } = useGauge(initialValue);
+  const { gauge, decreaseGauge, isSuccess } = useGauge(initialValue);
+
+  const handleCloseModal = () => {
+    window.location.reload(); // 간단히 페이지를 새로고침하여 모달을 닫음
+  };
 
   return (
     <div>
@@ -19,6 +24,8 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
       <button onClick={decreaseGauge} className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
         Decrease Gauge
       </button>
+
+      {isSuccess && <Modal message="The gauge is fully depleted." onClose={handleCloseModal} />}
     </div>
   );
 };
