02e5318ea4c3f331c9b6bbb6e4a78d1ab925558c
정호연 <jea7730@gmail.com>
2024-08-20 17:24:45 +0900

    :hammer: 정호연 : click한 item들이 나타나도록 하기

--- front/src/components/InventoryModal.tsx
+++ front/src/components/InventoryModal.tsx
@@ -1,22 +1,24 @@
+import { StaticImageData } from 'next/image';
 import React from 'react';
 
 type InventoryModalProps = {
+  items: { src: StaticImageData; name: string }[];
   onClose: () => void;
-  inventory: string[]; // 인벤토리 아이템 리스트
 };
 
-const InventoryModal: React.FC<InventoryModalProps> = ({ onClose, inventory }) => {
+const InventoryModal: React.FC<InventoryModalProps> = ({ items, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded shadow-lg w-96 h-96 relative">
+      <div className="bg-white p-6 rounded shadow-lg w-96 h-auto relative">
         <h2 className="text-xl font-bold mb-4">인벤토리</h2>
-        <ul className="list-disc list-inside">
-          {inventory.map((item, index) => (
-            <li key={index} className="mb-2">
-              {item}
-            </li>
+        <div className="grid grid-cols-3 gap-4">
+          {items.map((item, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <img src={item.src.src} alt={item.name} className="w-16 h-16" />
+              <span className="mt-2">{item.name}</span>
+            </div>
           ))}
-        </ul>
+        </div>
         <button
           onClick={onClose}
           className="absolute top-2 right-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
