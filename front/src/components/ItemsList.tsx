32e8988b3659b7bd5155937964855186295866ee
정호연 <jea7730@gmail.com>
2024-08-20 17:01:53 +0900

    :bug: 정호연 : item 하나만 담아도 다시 시작되는 오류 수정

--- front/src/components/ItemsList.tsx
+++ front/src/components/ItemsList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import chick from '../public/chick.png';
 import nest from '../public/nest.png';
 import ribbon from '../public/ribbon.png';
@@ -7,27 +7,31 @@ import sunglasses1 from '../public/sunglasses-1.png';
 import sunglasses2 from '../public/sunglasses-2.png';
 
 type ItemsListProps = {
+  visibleItems: string[];
   onItemClicked: (itemName: string) => void;
 };
 
-const ItemsList: React.FC<ItemsListProps> = ({ onItemClicked }) => {
+const ItemsList: React.FC<ItemsListProps> = ({ visibleItems, onItemClicked }) => {
+  // 각 아이템의 이미지 경로를 StaticImageData 타입으로 정의
+  const itemsMap: Record<string, StaticImageData> = {
+    chick,
+    nest,
+    ribbon,
+    'sunglasses-1': sunglasses1,
+    'sunglasses-2': sunglasses2,
+  };
+
   return (
     <div className="flex space-x-4 mt-4 animate-bounce">
-      <Image src={chick} alt="아이템" className="w-16 h-16 cursor-pointer" onClick={() => onItemClicked('chick')} />
-      <Image src={nest} alt="아이템" className="w-16 h-16 cursor-pointer" onClick={() => onItemClicked('nest')} />
-      <Image src={ribbon} alt="아이템" className="w-16 h-16 cursor-pointer" onClick={() => onItemClicked('ribbon')} />
-      <Image
-        src={sunglasses1}
-        alt="아이템"
-        className="w-16 h-16 cursor-pointer"
-        onClick={() => onItemClicked('sunglasses-1')}
-      />
-      <Image
-        src={sunglasses2}
-        alt="아이템"
-        className="w-16 h-16 cursor-pointer"
-        onClick={() => onItemClicked('sunglasses-2')}
-      />
+      {visibleItems.map((itemName) => (
+        <Image
+          key={itemName}
+          src={itemsMap[itemName]}
+          alt="아이템"
+          className="w-16 h-16 cursor-pointer"
+          onClick={() => onItemClicked(itemName)}
+        />
+      ))}
     </div>
   );
 };
