35420662ea866aeaf3465581b70d5e64974eb7a8
정호연 <jea7730@gmail.com>
2024-08-20 17:32:24 +0900

    :wastebasket: 정호연 : 코드 리펙토링 조립

--- front/src/components/GaugeBar.tsx
+++ front/src/components/GaugeBar.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGauge } from '../hooks/useGauge';
+import { useInventory } from '../hooks/useInventory';
 import Kong from './Kong';
 import Gauge from './Gauge';
 import ItemsList from './ItemsList';
 import InventoryModal from './InventoryModal';
-import Image, { StaticImageData } from 'next/image';
-import bag from '../public/bag.png';
+import InventoryButton from './InventoryButton';
+import LevelDisplay from './LevelDisplay';
 
 type GaugeBarProps = {
   initialValue: number;
@@ -13,9 +14,8 @@ type GaugeBarProps = {
 
 const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
   const { gauge, handleClick, isShaking } = useGauge(initialValue);
+  const { inventory, showItems, setShowItems, addItemToInventory } = useInventory();
   const [isInventoryOpen, setIsInventoryOpen] = useState(false);
-  const [showItems, setShowItems] = useState(false);
-  const [inventory, setInventory] = useState<{ src: StaticImageData; name: string }[]>([]);
 
   useEffect(() => {
     if (gauge === 0) {
@@ -23,29 +23,17 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
         setShowItems(true);
       }, 300);
     }
-  }, [gauge]);
-
-  const handleItemClick = (item: { src: StaticImageData; name: string }) => {
-    setInventory((prev) => [...prev, item]);
-    if (inventory.length + 1 === 5) {
-      setShowItems(false); // 모든 아이템이 담기면 아이템들을 사라지게 함
-    }
-  };
+  }, [gauge, setShowItems]);
 
   return (
     <div className="relative flex justify-center min-h-screen">
       <div className="flex flex-col items-center justify-center">
-        <div className="font-bold mb-4">Lv.99 욱재몬</div>
+        <LevelDisplay />
         <Gauge gauge={gauge} />
         {!showItems && inventory.length < 5 && <Kong onClick={handleClick} isShaking={isShaking} />}
-        {showItems && <ItemsList onItemClick={handleItemClick} inventory={inventory} />}
+        {showItems && <ItemsList onItemClick={addItemToInventory} inventory={inventory} />}
       </div>
-      <Image
-        src={bag}
-        alt="인벤토리"
-        onClick={() => setIsInventoryOpen(true)}
-        className="absolute top-4 right-4 w-24 h-24 transform transition-transform duration-300 hover:scale-110"
-      />
+      <InventoryButton onClick={() => setIsInventoryOpen(true)} />
       {isInventoryOpen && <InventoryModal items={inventory} onClose={() => setIsInventoryOpen(false)} />}
     </div>
   );
