d74fe608d4e49cdb21a03315f0a50e466559ee58
정호연 <jea7730@gmail.com>
2024-08-20 11:32:26 +0900

    :adhesive_bandage: 정호연 : 영어 > 한국어로 변경

--- front/src/components/Modal.tsx
+++ front/src/components/Modal.tsx
@@ -9,10 +9,10 @@ const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-5 rounded shadow-lg text-center text-black">
-        <h2 className="text-xl font-bold mb-4">Success!</h2>
+        <h2 className="text-xl font-bold mb-4">성공!</h2>
         <p>{message}</p>
         <button onClick={onClose} className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
-          Close
+          다시하기
         </button>
       </div>
     </div>
