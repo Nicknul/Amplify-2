da4b64db8b9a0704c263946dc294bedafb7a90dc
정호연 <jea7730@gmail.com>
2024-08-20 15:58:30 +0900

    :lipstick: 정호연 : style 수정

--- front/src/components/Modal.tsx
+++ front/src/components/Modal.tsx
@@ -25,12 +25,12 @@ const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       {showConfetti && <Confetti />} {/* 처음 모달이 나타날 때만 폭죽 표시 */}
       <div
-        className="backdrop-blur-xl bg-white/30 p-5 rounded-xl shadow-lg text-center relative"
+        className="w-72 h-64 backdrop-blur-xl bg-white/30 p-5 rounded-xl shadow-lg text-center relative flex flex-col items-center justify-evenly"
         style={{ zIndex: 10 }}
       >
-        <h2 className="text-xl font-bold mb-4">오늘은 치킨이닭!</h2>
+        <h2 className="text-xl font-bold">효과는 굉장했다!</h2>
         <p>{message}</p>
-        <button onClick={handleClose} className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+        <button onClick={handleClose} className="w-16 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
           닫기
         </button>
       </div>
