497a8d447dced382fe91c0b8dc5af00deb5b1460
정호연 <jea7730@gmail.com>
2024-08-20 17:22:54 +0900

    :sparkles: 정호연 : items 기능

--- front/src/components/GaugeBar.tsx
+++ front/src/components/GaugeBar.tsx
@@ -3,9 +3,9 @@ import { useGauge } from '../hooks/useGauge';
 import Kong from './Kong';
 import Gauge from './Gauge';
 import ItemsList from './ItemsList';
-import Image from 'next/image';
-import bag from '../public/bag.png';
 import InventoryModal from './InventoryModal';
+import Image, { StaticImageData } from 'next/image';
+import bag from '../public/bag.png';
 
 type GaugeBarProps = {
   initialValue: number;
@@ -15,14 +15,7 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
   const { gauge, handleClick, isShaking } = useGauge(initialValue);
   const [isInventoryOpen, setIsInventoryOpen] = useState(false);
   const [showItems, setShowItems] = useState(false);
-  const [inventory, setInventory] = useState<string[]>([]); // 인벤토리 상태 관리
-  const [visibleItems, setVisibleItems] = useState<string[]>([
-    'chick',
-    'nest',
-    'ribbon',
-    'sunglasses-1',
-    'sunglasses-2',
-  ]); // 화면에 보이는 아이템들 관리
+  const [inventory, setInventory] = useState<{ src: StaticImageData; name: string }[]>([]);
 
   useEffect(() => {
     if (gauge === 0) {
@@ -32,17 +25,10 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
     }
   }, [gauge]);
 
-  // 아이템을 인벤토리에 추가하는 함수
-  const addItemToInventory = (itemName: string) => {
-    setInventory((prevInventory) => [...prevInventory, itemName]);
-    setVisibleItems((prevVisibleItems) => prevVisibleItems.filter((item) => item !== itemName));
-
-    // 모든 아이템이 수집되면 추가적인 동작 수행 (예: 화면 리셋)
-    if (visibleItems.length === 1) {
-      // 모든 아이템이 수집된 후 동작
-      setTimeout(() => {
-        window.location.reload(); // 예: 페이지를 리로드
-      }, 1000);
+  const handleItemClick = (item: { src: StaticImageData; name: string }) => {
+    setInventory((prev) => [...prev, item]);
+    if (inventory.length + 1 === 5) {
+      setShowItems(false); // 모든 아이템이 담기면 아이템들을 사라지게 함
     }
   };
 
@@ -51,9 +37,8 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
       <div className="flex flex-col items-center justify-center">
         <div className="font-bold mb-4">Lv.99 욱재몬</div>
         <Gauge gauge={gauge} />
-        {!showItems && <Kong onClick={handleClick} isShaking={isShaking} />}
-        {showItems && <ItemsList visibleItems={visibleItems} onItemClicked={addItemToInventory} />}{' '}
-        {/* 수정된 아이템 리스트 */}
+        {!showItems && inventory.length < 5 && <Kong onClick={handleClick} isShaking={isShaking} />}
+        {showItems && <ItemsList onItemClick={handleItemClick} inventory={inventory} />}
       </div>
       <Image
         src={bag}
@@ -61,9 +46,7 @@ const GaugeBar: React.FC<GaugeBarProps> = ({ initialValue }) => {
         onClick={() => setIsInventoryOpen(true)}
         className="absolute top-4 right-4 w-24 h-24 transform transition-transform duration-300 hover:scale-110"
       />
-      {isInventoryOpen && (
-        <InventoryModal onClose={() => setIsInventoryOpen(false)} inventory={inventory} /> // 인벤토리 상태 전달
-      )}
+      {isInventoryOpen && <InventoryModal items={inventory} onClose={() => setIsInventoryOpen(false)} />}
     </div>
   );
 };
