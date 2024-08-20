d0e2bcae6d45bc63c9406d591f2021a43948b73d
정호연 <jea7730@gmail.com>
2024-08-20 15:28:23 +0900

    :lipstick: 정호연 : Modal style 수정

--- front/src/components/Modal.tsx
+++ front/src/components/Modal.tsx
@@ -24,7 +24,10 @@ const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       {showConfetti && <Confetti />} {/* 처음 모달이 나타날 때만 폭죽 표시 */}
-      <div className="bg-white p-5 rounded shadow-lg text-center relative" style={{ zIndex: 10 }}>
+      <div
+        className="backdrop-blur-xl bg-white/30 p-5 rounded-xl shadow-lg text-center relative"
+        style={{ zIndex: 10 }}
+      >
         <h2 className="text-xl font-bold mb-4">Success!</h2>
         <p>{message}</p>
         <button onClick={handleClose} className="mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
