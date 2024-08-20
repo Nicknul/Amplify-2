bade4fb628006c56128073af67172b6de938c076
정호연 <jea7730@gmail.com>
2024-08-20 14:22:20 +0900

    :fire: 정호연 : <h1> 삭제

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -6,7 +6,6 @@ import GaugeBar from '../components/GaugeBar';
 const Home: React.FC = () => {
   return (
     <div className="p-5">
-      <h1 className="text-2xl font-bold mb-4">Gauge Bar Example</h1>
       <GaugeBar initialValue={100} />
     </div>
   );
