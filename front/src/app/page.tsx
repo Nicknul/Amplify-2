2ea21311a2c74c54ce16209b11ba4880fb5b12cb
정호연 <jea7730@gmail.com>
2024-08-20 10:45:28 +0900

    :hammer: 정호연 : image module 조립

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import kongImage from '../public/kong.png';
 
 export default function Home() {
   return (
     <>
-      <button className="bg-blue-500 px-4 py-2 m-5 text-white rounded-lg">Hello world</button>
+      <button className="bg-blue-500 px-4 py-2 m-5 text-white rounded-lg hover:bg-blue-600">Hello world</button>
+      <Image src={kongImage} alt="미스터 공" />
     </>
   );
 }
