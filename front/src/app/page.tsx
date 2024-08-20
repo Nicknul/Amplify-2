a0d5e091310967ac8d08adcb8f8c281737699c0a
정호연 <jea7730@gmail.com>
2024-08-20 10:47:11 +0900

    :adhesive_bandage: 정호연 : image module 주석 처리

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <button className="bg-blue-500 px-4 py-2 m-5 text-white rounded-lg hover:bg-blue-600">Hello world</button>
-      <Image src={kongImage} alt="미스터 공" />
+      {/* <Image src={kongImage} alt="미스터 공" /> */}
     </>
   );
 }
